fcf11201c3a53ba1c5579a75c40a6c8b5736e34c
Ojcar Sánchez <jfosanchez@gmail.com>
2020-03-01 19:30:09 +0100

    dw

--- src/components/buttonLinks.js
+++ src/components/buttonLinks.js
@@ -1,7 +1,6 @@
 import React from "react"
 
 import { Link } from "gatsby"
-import classnames from "classnames"
 
 const links = [
   {
@@ -17,14 +16,12 @@ const links = [
     path: "/page-3",
   },
 ]
-const selectedLinkClassName = link =>
-  classnames({ selected: link === window.location.pathname })
 
 const Buttons = () => {
   return (
     <div className="Links">
       {links.map(({ path, label }) => (
-        <Link to={path} key={path} className={selectedLinkClassName(path)}>
+        <Link to={path} key={path}>
           {label}
         </Link>
       ))}
